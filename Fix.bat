4fade285a872b3e2e61649a7b95d41f40f7aa23d
QwertyHJKL1234 <64031126+QwertyHJKL1234@users.noreply.github.com>
2024-07-19 10:50:57 +0100

    Attempt to delete Microsoft-Reccomended File location

--- Fix.bat
+++ Fix.bat
@@ -1,5 +1,7 @@
 @echo off
 del c:\Windows\System32\Drivers\CrowdStrike\C00000291*.sys
 echo Deleted c:\Windows\System32\Drivers\CrowdStrike\C00000291*.sys
+del c:\Windows\System\System32\Drivers\CrowdStrike\C00000291*.sys
+echo Attempted to delete Microsoft-Reccomended path to the file.
 echo done
 pause
